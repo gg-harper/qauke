8bf6652f9466938d3c03013cfeb240b2b8941100
gg-harper <osstia@list.ru>
2024-08-21 23:47:21 +0300

    init

---
+++ src/main/java/org/harper/qauke/QaukeApplication.java
@@ -0,0 +1,13 @@
+package org.harper.qauke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/harper/qauke/QaukeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harper.qauke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
